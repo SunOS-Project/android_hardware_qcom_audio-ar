371002bdb4f0c6956d39ccdb6810dc0004d6019c
gmanam <quic_gmanam@quicinc.com>
2023-04-27 21:39:49 +0530

    hal: aidl service
    Change-Id: I1a959da4a755eed5183e6d79d82147f24101320b

---
+++ hal/service/Service.cpp
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#define LOG_NDEBUG 0
+#define LOG_TAG "AHAL_Service"
+
+#include <cstdlib>
+#include <ctime>
+#include <dlfcn.h>
+
+#include <algorithm>
+
+#include<vector>
+#include<string>
+#include <chrono>
+
+#include <log/log.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+#include <android-base/logging.h>
+#include <cutils/properties.h>
+#include "ConfigManager.h"
+
+// TODO Remove it
+static bool gFatalIfMandatoryInterfaceMissing = false;
+
+static bool registerServiceImplementation(const Interface & interface) {
+    auto libraryName = interface.libraryName;
+    auto interfaceMethod = interface.method;
+    void* handle = dlopen(libraryName.c_str(), RTLD_LAZY);
+    if (handle == nullptr) {
+        const char* error = dlerror();
+        ALOGE("Failed to dlopen %s: %s", libraryName.c_str(), error != nullptr ? error : "unknown error");
+        return false;
+    }
+    auto instantiate = reinterpret_cast<binder_status_t (*)()>
+                                (dlsym(handle, interfaceMethod.c_str()));
+    if (instantiate == nullptr) {
+        const char* error = dlerror();
+        ALOGE("Factory function %s not found in libName %s: %s", interfaceMethod.c_str(), libraryName.c_str(),
+              error != nullptr ? error : "unknown error");
+        dlclose(handle);
+        return false;
+    }
+    return (instantiate() == STATUS_OK);
+}
+
+void registerInterfaces(const Interfaces &interfaces) {
+     for (const auto& interface : interfaces) {
+            if (registerServiceImplementation(interface)) {
+                ALOGI("successfully registered %s", interface.toString().c_str());
+            } else if(interface.mandatory) { //TODO
+                LOG_ALWAYS_FATAL_IF(gFatalIfMandatoryInterfaceMissing, "failed to register %s ", interface.toString().c_str());
+            } else {
+                ALOGW("failed to register optional %s ", interface.toString().c_str());
+            }
+    }
+}
+
+bool registerFromConfigs() {
+    auto interfaces = parseInterfaces();
+    registerInterfaces(interfaces);
+    return !interfaces.empty();
+}
+
+void registerDefaultInterfaces() {
+    Interfaces defaultInterfaces = {
+        {
+            .name = "audiohal",
+            .libraryName = "libaudiocorehal.qti.so",
+            .method = "registerService",
+            .mandatory = true
+        },
+        {
+            .name = "audioeffecthal",
+            .libraryName = "libaudioeffecthal.qti.so",
+            .method = "registerService",
+            .mandatory = true
+        },
+        {
+            .name = "sthal",
+            .libraryName = "android.hardware.soundtrigger.audio-impl.so",
+            .method = "createISoundTriggerFactory",
+            .mandatory = true
+        },
+        {
+            .name = "bthal",
+            .libraryName = "android.hardware.bluetooth.audio-impl.so",
+            .method = "createIBluetoothAudioProviderFactory",
+            .mandatory = false
+        },
+    };
+
+    registerInterfaces(defaultInterfaces);
+}
+
+void registerAvailableInterfaces() {
+    if (!registerFromConfigs()) {
+        ALOGI("registerDefaultInterfaces");
+        registerDefaultInterfaces();
+    }
+}
+
+void setLogSeverity() {
+    char value[PROPERTY_VALUE_MAX];
+    int logLevel = 2;
+    // system/libbase/include/android-base/logging.h
+    if (property_get("vendor.audio.hal.loglevel", value, "2")) {
+        int level = atoi(value);
+        if (level >= 0 && level <= 6)
+            logLevel = level;
+    }
+
+    android::base::SetMinimumLogSeverity(static_cast<android::base::LogSeverity>(logLevel));
+}
+
+int main() {
+    auto startTime = std::chrono::steady_clock::now();
+    // Random values are used in the implementation.
+    std::srand(std::time(nullptr));
+    setLogSeverity();
+
+    ABinderProcess_setThreadPoolMaxThreadCount(16);
+    registerAvailableInterfaces();
+    auto endTime = std::chrono::steady_clock::now();
+    float timeTaken = std::chrono::duration_cast<std::chrono::duration<float>>(
+                         endTime - startTime).count();
+    ALOGI("registration took %.2f seconds ", timeTaken);
+    ABinderProcess_joinThreadPool();
+    return EXIT_FAILURE;
+}
