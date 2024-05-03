c41f63b6e08afe4e29e4b0a874bd2dd469c632ca
Ajender Reddy <quic_ajender@quicinc.com>
2024-05-03 20:46:20 +0530

    core: revert the observable position as non-retrograde
    The mismatch in expectation and implementation of audio
    framework is causing issue while resuming the playback
    in some cases.
    
    Change-Id: Ib82cc37cea473844e8ba78845fc73f3ad188feda

--- hal/core/platform/AudioUsecase.cpp
+++ hal/core/platform/AudioUsecase.cpp
@@ -696,8 +696,9 @@ int64_t CompressPlayback::getPositionInFrames(pal_stream_handle_t* palHandle) {
 
 void CompressPlayback::onFlush() {
     // on flush SPR module is reset to 0. Hence, we cache the DSP frames
-    mTotalDSPFrames = mTotalDSPFrames + mPrevFrames;
-    mPrevFrames = 0;
+    // uncomment below to have observable position as non-retrograde
+    // mTotalDSPFrames = mTotalDSPFrames + mPrevFrames;
+    // mPrevFrames = 0;
 }
 
 // [CompressPlayback End]
@@ -754,8 +755,9 @@ int64_t PcmOffloadPlayback::getPositionInFrames(pal_stream_handle_t* palHandle) 
 
 void PcmOffloadPlayback::onFlush() {
     // on flush SPR module is reset to 0. Hence, we cache the DSP frames
-    mTotalDSPFrames = mTotalDSPFrames + mPrevFrames;
-    mPrevFrames = 0;
+    // uncomment below to have observable position as non-retrograde
+    // mTotalDSPFrames = mTotalDSPFrames + mPrevFrames;
+    // mPrevFrames = 0;
 }
 
 // [PcmOffloadPlayback End]
